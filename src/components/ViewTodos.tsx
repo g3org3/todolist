7d2a0eee112c0b6874579dc41c87717be73c7c6f
george <7jagjag@gmail.com>
2022-10-04 06:46:41 +0200

    add search and filter

--- src/components/ViewTodos.tsx
+++ src/components/ViewTodos.tsx
@@ -1,7 +1,7 @@
-import { Flex, Badge, Button, Text, useToast } from '@chakra-ui/react'
+import { Flex, Badge, Button, Text, useToast, Input } from '@chakra-ui/react'
 import autoAnimate from '@formkit/auto-animate'
 import { DateTime } from 'luxon'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import { TodoOut, trpc } from 'utils/trpc'
 
@@ -12,6 +12,8 @@ interface Props {
 const ViewTodos = (props: Props) => {
   const ref = useRef(null)
   const toaster = useToast()
+  const [showAll, setShowAll] = useState(false)
+  const [search, setSearch] = useState('')
   const { invalidateQueries } = trpc.useContext()
   const todos = trpc.useQuery(['auth.todolist'])
   const doneTodo = trpc.useMutation('auth.done', {
@@ -44,66 +46,82 @@ const ViewTodos = (props: Props) => {
     doneTodo.mutate({ id, isChecked })
   }
 
-  return (
-    <Flex flexDir="column" ref={ref} flex="1" overflow="auto" gap={2}>
-      {todos.data?.map((x, i) => (
-        <Flex
-          alignItems={{ base: 'unset', md: 'center' }}
-          gap={{ base: 0, md: 4 }}
-          boxShadow="md"
-          border="1px solid #eee"
-          bg="white"
-          borderRadius="10px"
-          key={x.id}
-          position="relative"
-          flexDir={{ base: 'column', md: 'row' }}
-        >
-          <Badge position="absolute" bottom="0" right="0" fontSize="10px">
-            {x.id}
-          </Badge>
-          <Text
-            borderRadius="10px"
-            transition="background 300ms"
-            _hover={{ background: 'blue.100' }}
-            cursor="pointer"
-            onClick={() => props.onClickSelect(x)}
-            flex="1"
-            p={5}
-            fontSize="3xl"
-            textDecor={x.doneAt ? 'line-through' : undefined}
-          >
-            {x.title}
-          </Text>
-          {x.doneAt && (
-            <Badge position="absolute" top="0" right="0" colorScheme="green">
-              Done: {DateTime.fromJSDate(x.doneAt).toRelative()}
-            </Badge>
-          )}
-          <Flex>
-            <Button
-              fontSize="3xl"
-              colorScheme="green"
-              variant="ghost"
-              size="sm"
-              onClick={onDone(x.id, !x.doneAt)}
-            >
-              ✅
-            </Button>
+  const filteredTodos =
+    todos.data
+      ?.filter((x) => showAll || !x.doneAt)
+      .filter((x) => !search || x.title.toLowerCase().includes(search.toLowerCase())) || []
 
-            <Button
-              mr={5}
+  return (
+    <>
+      <Flex gap={4}>
+        <Button isActive={showAll} onClick={() => setShowAll(!showAll)} colorScheme="green" variant="outline">
+          show all
+        </Button>
+        <Input value={search} onChange={(e) => setSearch(e.target.value)} placeholder="search" />
+        <Button onClick={() => setSearch('')} variant="outline">
+          clear
+        </Button>
+      </Flex>
+      <Flex flexDir="column" ref={ref} flex="1" overflow="auto" gap={2}>
+        {filteredTodos.map((x) => (
+          <Flex
+            alignItems={{ base: 'unset', md: 'center' }}
+            gap={{ base: 0, md: 4 }}
+            boxShadow="md"
+            border="1px solid #eee"
+            bg="white"
+            borderRadius="10px"
+            key={x.id}
+            position="relative"
+            flexDir={{ base: 'column', md: 'row' }}
+          >
+            <Badge position="absolute" bottom="0" right="0" fontSize="10px">
+              {x.id}
+            </Badge>
+            <Text
+              borderRadius="10px"
+              transition="background 300ms"
+              _hover={{ background: 'blue.100' }}
+              cursor="pointer"
+              onClick={() => props.onClickSelect(x)}
+              flex="1"
+              p={5}
               fontSize="3xl"
-              colorScheme="red"
-              variant="ghost"
-              size="sm"
-              onClick={onDelete(x.id)}
+              textDecor={x.doneAt ? 'line-through' : undefined}
             >
-              ❌
-            </Button>
+              {x.title}
+            </Text>
+            {x.doneAt && (
+              <Badge position="absolute" top="0" right="0" colorScheme="green">
+                Done: {DateTime.fromJSDate(x.doneAt).toRelative()}
+              </Badge>
+            )}
+            <Flex>
+              <Button
+                fontSize="3xl"
+                colorScheme="green"
+                variant="ghost"
+                size="sm"
+                onClick={onDone(x.id, !x.doneAt)}
+              >
+                ✅
+              </Button>
+
+              <Button
+                mr={5}
+                fontSize="3xl"
+                colorScheme="red"
+                variant="ghost"
+                size="sm"
+                onClick={onDelete(x.id)}
+              >
+                ❌
+              </Button>
+            </Flex>
           </Flex>
-        </Flex>
-      ))}
-    </Flex>
+        ))}
+      </Flex>
+    </>
   )
 }
 
